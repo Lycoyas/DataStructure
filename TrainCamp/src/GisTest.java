831ebfcdeee10ef71fd0ed9be6e8c4e45e3fda69
Lycoyas <1341648608@qq.com>
2023-03-06 22:38:29 +0800

    third commit

--- TrainCamp/src/GisTest.java
+++ TrainCamp/src/GisTest.java
@@ -5,5 +5,7 @@
 public class GisTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("git 2");
+        System.out.println("git 3");
     }
 }
