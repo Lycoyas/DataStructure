b86cce03fa84ad81894ba4bd644ca4e0e05889ef
Lycoyas <37526250+Lycoyas@users.noreply.github.com>
2023-03-06 23:02:06 +0800

    pull commit

--- TrainCamp/src/GisTest.java
+++ TrainCamp/src/GisTest.java
@@ -14,7 +14,7 @@ public class GisTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
-
+        System.out.println("pull test");
 
 
     }
